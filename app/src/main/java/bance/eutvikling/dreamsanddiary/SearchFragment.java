3d04c127054bff76c8773cda2b431050ae4077c9
Bance <kalianasu@gmail.com>
2022-06-14 09:31:10 +0200

    +Search button

--- app/src/main/java/bance/eutvikling/dreamsanddiary/SearchFragment.java
+++ app/src/main/java/bance/eutvikling/dreamsanddiary/SearchFragment.java
@@ -2,11 +2,14 @@ package bance.eutvikling.dreamsanddiary;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class SearchFragment extends Fragment {
@@ -20,5 +23,13 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btn = view.findViewById(R.id.btnSearch);
+
     }
 }
