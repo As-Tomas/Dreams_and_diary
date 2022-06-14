ddadeeab3795fe814937cb833490cbb04f136a09
Bance <kalianasu@gmail.com>
2022-06-14 10:59:23 +0200

    + List view, + Search button onClick

--- app/src/main/java/bance/eutvikling/dreamsanddiary/SearchFragment.java
+++ app/src/main/java/bance/eutvikling/dreamsanddiary/SearchFragment.java
@@ -10,9 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
 
 
 public class SearchFragment extends Fragment {
+
+    ArrayList<Dream> listOfDreams;
+    ListView list;
+    ArrayListAdapter adapter;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -22,7 +31,14 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        listOfDreams = new ArrayList();
+        adapter = new ArrayListAdapter(listOfDreams, view.getContext());
+        list = view.findViewById(R.id.listResults);
+        list.setAdapter(adapter);
+
+        return view;
     }
 
     @Override
@@ -30,6 +46,18 @@ public class SearchFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         Button btn = view.findViewById(R.id.btnSearch);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                TextInputEditText searchEditText = getActivity().findViewById(R.id.search);
+                CharSequence searchFor = searchEditText.getText();
+
+
+
+            }
+        });
 
     }
 }
